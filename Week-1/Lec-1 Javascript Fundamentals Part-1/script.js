4700fac4004d9ac8a2cda56423d8f6f05c3118d6
larssantura <larssantura@gmail.com>
2024-11-01 13:05:41 +0200

    lec1

--- Week-1/Lec-1 Javascript Fundamentals Part-1/script.js
+++ Week-1/Lec-1 Javascript Fundamentals Part-1/script.js
@@ -1,22 +1,53 @@
 // Variable types, Naming rules and conventions
 
+// const name = "Ehab"; //string constant
+// console.log(name);
+// let age = 40; //number variable
+
+//-------------------------------------------------------------------
 // basic operators in JS
+//-------------------------------------------------------------------
 
+// let x = 2;
+// let y = 4;
+
+// console.log("Addition: ", x + y);
+// console.log("Subtraction: ", x - y);
+// console.log("Multiplication: ", x * y);
+// console.log("Division: ", x / y);
+// console.log("modulo: ", x % y);
+// console.log("power: ", x ** y);
+
+//-------------------------------------------------------------------
 // Assignment Operators
+//-------------------------------------------------------------------
 
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--;
+// x--;
+// console.log(x);
+
+//-------------------------------------------------------------------
 // Comparison operators
+//-------------------------------------------------------------------
 
 //  <, >, <=, >=, !, !=, ==, ===, =
 
-
+//-------------------------------------------------------------------
 // Operator Precedence
+//-------------------------------------------------------------------
 
-console.log(2 + 4 / 2 * 5)
+// () > division and multiplication > addition and subtraction
 
-// Coding challenge: 
+// =====================================================================================================
+
+// Coding challenge:
 // Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
 
-// 1. Store Mark's and John's mass and height in variables
+// 1. Store Mark's and John's mass and height in variables 70kg, 1.7m
 // 2. Calculate both their BMIs using the formula (you can even implement both versions)
 // 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 
